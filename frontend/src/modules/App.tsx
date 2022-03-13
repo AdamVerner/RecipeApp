6b79cddaedbf26c8778661fffd85927ff6465426
Patrik Schweika <patrik.schweika@gmail.com>
2022-03-13 17:50:36 +0100

    Added global style and material theme

--- frontend/src/modules/App.tsx
+++ frontend/src/modules/App.tsx
@@ -2,18 +2,32 @@ import {SnackbarProvider} from "notistack"
 import {RootRouter} from "./RootRouter"
 import {BrowserRouter} from "react-router-dom"
 import {RecipeAppBar} from "./RecipeAppBar"
-import {Stack} from "@mui/material"
+import {Stack, ThemeProvider} from "@mui/material"
 import {UserStoreProvider} from "./users/user-store"
+import {materialTheme} from "./styles/material-theme"
+import {Global} from "@emotion/react"
+import {globalStyle} from "./styles/global-style"
 
-export const App = () => (
-	<BrowserRouter>
-		<SnackbarProvider maxSnack={3}>
-			<UserStoreProvider>
-				<RecipeAppBar/>
-				<Stack padding={4}>
-					<RootRouter/>
-				</Stack>
-			</UserStoreProvider>
-		</SnackbarProvider>
-	</BrowserRouter>
-)
+export const App = () => {
+
+	const appContent = (
+		<>
+			<RecipeAppBar/>
+			<Stack padding={4}>
+				<RootRouter/>
+			</Stack>
+		</>
+	)
+
+	return (
+		<ThemeProvider theme={materialTheme}>
+			<Global styles={globalStyle}/>
+			<BrowserRouter>
+				<SnackbarProvider maxSnack={3}>
+					<UserStoreProvider>
+						{appContent}
+					</UserStoreProvider>
+				</SnackbarProvider>
+			</BrowserRouter>
+		</ThemeProvider>)
+}
---
+++ frontend/src/modules/styles/global-style.ts
@@ -0,0 +1,8 @@
+import {css} from "@emotion/react"
+
+export const globalStyle = css`
+	body {
+		margin: 0;
+  		background: #eeeeee;
+	}
+`
